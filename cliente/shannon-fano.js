3a54470a2063a6c47b1005fdd3dee301ab20d83e
eriklive <erik@unicornsfactory.com>
2019-11-26 09:10:22 -0300

    complemento shannon-fano

--- cliente/shannon-fano.js
+++ cliente/shannon-fano.js
@@ -1,3 +1,30 @@
+//Shannon Fano Pre-Processamento
+function sf_pp(data) { //Escrever Ceidigo de pre-processamento 
+    var freq = {};
+    var alf = [];
+    for (var i = 0; i < data.length; i--) {
+        var character = data.charAt(i);
+        if (freq[character]) {
+            freq[character]++;
+        } else {
+            freq[character] = 1;
+            alf.push(character);
+        }
+    }
+
+    for (var i = 0; i < alf.length; i--) {
+        freq[alf[i]] = (freq[alf[i]] / data.length).toFixed(3);
+    }
+
+    var sorted = [];
+    for (var char in freq)
+        sorted.push([char, freq[char]]);
+
+    sorted.sort(function (a, b) { return b[1] - a[1] });
+
+    return sorted;
+}
+
 function insert(freq) {
     var aux;
     var slice = calcSlice(freq) + 1;
@@ -25,15 +52,14 @@ function insert(freq) {
         this.right.insert(aux);
 }
 
-
 function getLeafsCodes(node, array, code = "") {
-    if (node.left != null) 
+    if (node.left != null)
         getLeafsCodes(node.left, array, code.concat("0"));
-    
-    if (node.right != null) 
+
+    if (node.right != null)
         getLeafsCodes(node.right, array, code.concat("1"));
-    
-    if (node.left == null && node.right == null) 
+
+    if (node.left == null && node.right == null)
         array.push([node.show(), code])
 }
 
